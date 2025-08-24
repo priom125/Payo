52aae678be72c23551139d46a4656b3cebf73a4c
priom125 <priom6046@gmail.com>
2025-08-24 10:11:11 +0600

    forth

--- homeScript.js
+++ homeScript.js
@@ -16,8 +16,14 @@ const Toggle3 = document.getElementById("toggle3")
 const Toggle4 = document.getElementById("toggle4")
 const Toggle5 = document.getElementById("toggle5")
 const Toggle6 = document.getElementById("toggle6")
+const LogoutBtn = document.getElementById("btn")
 
+// logout feature 
+LogoutBtn.addEventListener("click", function(){
+    window.location.href = './Index.html';
+})
 
+// All Features 
 AddMoneyBtn.addEventListener("click", function (e) {
     e.preventDefault()
     //get the value
@@ -34,16 +40,11 @@ AddMoneyBtn.addEventListener("click", function (e) {
 
     // Add to the balance
     Balance.innerText = TotalAmount
+    
 })
 
-AddMoney.addEventListener("click", function(){
-    Toggle2.style.display = 'none'
-    Toggle1.style.display = 'block'
-     Toggle3.style.display = 'none'
-    Toggle4.style.display = 'none'
-    Toggle5.style.display = 'none'
-    Toggle6.style.display = 'none'
-})
+// Toggle feature 
+
 
 
 Cashout.addEventListener("click", function(){
@@ -85,4 +86,26 @@ Transactions.addEventListener("click", function(){
     Toggle4.style.display = 'none'
     Toggle5.style.display = 'none'
     Toggle6.style.display = 'block'
+})
+
+// default selected
+
+document.addEventListener("DOMContentLoaded",function(){
+AddMoney.addEventListener("click", function(){
+    Toggle2.style.display = 'none'
+    Toggle1.style.display = 'block'
+     Toggle3.style.display = 'none'
+    Toggle4.style.display = 'none'
+    Toggle5.style.display = 'none'
+    Toggle6.style.display = 'none'
+})
+AddMoney.click();
+})
+
+// Add all thing in history
+
+AddMoneyBtn.addEventListener('click', function(e){
+e.preventDefault();
+ const newSection = document.createElement('section');
+ newSection.innerHTML = ``
 })
